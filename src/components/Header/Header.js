986532b0cad170b4f32e41d8068f65816968862e
rakibul58 <rhrahi14@gmail.com>
2022-10-25 12:31:20 +0600

    link to routes added andd nav style changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import logo from '../../assests/images/favicon-32x32.png';
-import { FaToggleOn , FaToggleOff} from 'react-icons/fa';
+import { FaToggleOn, FaToggleOff } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 
@@ -12,44 +12,50 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     const [dark, setDark] = useState(false);
     return (
-        <Navbar collapseOnSelect expand="lg" bg={dark?'dark':'light'} variant={dark?'dark':'light'}>
+        <Navbar collapseOnSelect expand="lg" bg={dark ? 'dark' : 'light'} variant={dark ? 'dark' : 'light'}>
             <Container>
-                <Navbar.Brand href="#home" className='navbar-title text-primary fw-semibold fs-2'>
-                    <img
-                        alt=""
-                        src={logo}
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-center"
-                    />{' '}
-                    Infinite Courses
-                </Navbar.Brand>
+                <Link className='text-decoration-none' to='/'>
+                    <Navbar.Brand href="#home" className='navbar-title text-primary fw-semibold fs-2'>
+                        <img
+                            alt=""
+                            src={logo}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-center"
+                        />{' '}
+                        Infinite Courses
+                    </Navbar.Brand>
+                </Link>
+
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mx-auto fw-semibold text-secondary">
-                        <Nav.Link href="#features">Courses</Nav.Link>
-                        <Nav.Link href="#pricing">FAQ</Nav.Link>
-                        <Nav.Link href="#pricing">Blog</Nav.Link>
+                    <Nav className="mx-auto d-flex gap-3">
+                        <Link className='text-secondary text-decoration-none fw-semibold' to="/courses">Courses</Link>
+                        <Link className='text-secondary text-decoration-none fw-semibold' to="/faq">FAQ</Link>
+                        <Link className='text-secondary text-decoration-none fw-semibold' to="/blog">Blog</Link>
                     </Nav>
                     <Nav>
-                        <div onClick={()=>setDark(!dark)} className={`fw-semibold fs-4 me-3 ${dark?'text-light':'text-dark'}`}>
+                        <div onClick={() => setDark(!dark)} className={`fw-semibold fs-4 me-3 ${dark ? 'text-light' : 'text-dark'}`}>
                             {dark ?
 
                                 <>
                                     Dark {' '}
-                                    <FaToggleOn/>
+                                    <FaToggleOn />
                                 </>
                                 :
                                 <>
                                     Light {' '}
-                                    <FaToggleOff/>
+                                    <FaToggleOff />
                                 </>
 
                             }
                         </div>
-                        
-                        <Button className='fw-semibold mx-2 my-1' variant='primary'>Login</Button>
-                        <Button className='fw-semibold my-1' variant='outline-primary'>Register</Button>
+                        <Link to='/login'>
+                            <Button className='fw-semibold mx-2 my-1' variant='primary'>Login</Button>
+                        </Link>
+                        <Link to='/register'>
+                            <Button className='fw-semibold my-1' variant='outline-primary'>Register</Button>
+                        </Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
