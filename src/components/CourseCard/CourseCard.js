2af76051dd6b17ed1c7a379aa2a3c2a6d4fea0a8
rakibul58 <rhrahi14@gmail.com>
2022-10-25 19:36:18 +0600

    course card added

---
+++ src/components/CourseCard/CourseCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+const CourseCard = ({ course }) => {
+    const { image, title , description} = course;
+    return (
+        <Col>
+            <Card className='h-100'>
+                <Card.Img className='img-fluid' variant="top" src={image} />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                        {description.length>20 ? description.slice(0,70)+'...' : description}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer className='bg-white border-0'>
+                    <Button variant='outline-primary' className="fw-semibold w-100 mb-2">Go to Course Details</Button>
+                </Card.Footer>
+            </Card>
+        </Col>
+
+    );
+};
+
+export default CourseCard;
--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
+import CourseCard from '../CourseCard/CourseCard';
 
 const Courses = () => {
     const courses = useLoaderData();
+    // console.log(courses);
     return (
-        <div>
-            <h1>This is courses :{courses.length}</h1>
-        </div>
+        <Container className='mb-5'>
+            <Row>
+                <Col lg="10">
+                    <h1 className='mt-5 text-primary text-opacity-75'>Our Courses</h1>
+
+                    <Row xs={1} md={2} lg={3} className="g-4 mt-3">
+                        {
+                            courses.map(course => <CourseCard
+                                key={course.id}
+                                course={course}
+                            ></CourseCard>)
+                        }
+                    </Row>
+
+                </Col>
+                <Col lg="2"></Col>
+            </Row>
+
+        </Container>
     );
 };
 
